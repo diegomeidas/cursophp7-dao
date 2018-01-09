2271b4d60001206d5ccf1ccdc11869e58b1678cb
diegomeidas <diegomeidas@hotmail.com>
2018-01-09 14:40:12 -0200

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -55,14 +55,52 @@ class Usuario{
 
     }
 
+    // 1 - metodo para buscar um usuario
     public function __toString(){
         return json_encode(array(
-           "idusuario"=>$this->getIdusuario(),
-           "deslogin"=>$this->getDeslogin(),
-           "dessenha"=>$this->getDessenha(),
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
             "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 
         ));
+    }
+
+    // 2 - metodo para buscar uma lista de usuarios
+    public static function getLista(){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+
+
+    // 3 - carrega uma lista de usuarios buscando pelo login
+    public static function search($Login){
+        $sql = new Sql();
+        //retorna do banco o usuario que o login tiver as letras passadas
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$Login."%"
+        ));
+    }
+
+    // 4 - carrega um usuario usando login e senha
+    public function login($login, $password){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :ID and dessenha = :PASS", array(
+            ":ID"=>$login,
+            ":PASS"=>$password
+        ));
+        //if(count($results) > 0)
+        if(isset($results)){
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }else{
+            throw new Exception("Login e/ou senha inválidos");
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -3,14 +3,37 @@
 require_once ("config.php");
 /*
 $selectSql = new Sql();
-
 $usuarios = $selectSql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
 
+/*
+// 1 - carrega um usuario
 $root = new Usuario();
 $root->loadById(3);
 echo $root;
+*/
+
+/*
+// 2 - carrega uma lista dos usuarios
+$lista = Usuario::getLista();   //por ser função estatica não precisa estanciar
+echo json_encode($lista);
+*/
+
+/*
+// 3 - carrega uma lista de usuarios buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+// 4 - carrega um usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$%");
+echo $usuario;
+
+
+
+
+
 
 ?>
